a236a61f51267602de615898632546eaf16da2dc
H82M19\Student <Student>
2024-09-08 07:44:04 +0700

    pipeline(week-01): init project, connect mariadb and setup basic

---
+++ src/main/java/vn/edu/iuh/fit/week01/controllers/ControlServlet.java
@@ -0,0 +1,23 @@
+package vn.edu.iuh.fit.week01.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "ControlServlet", value = "/control")
+public class ControlServlet  extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
